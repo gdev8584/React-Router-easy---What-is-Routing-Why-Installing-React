20647358630bead108ff4623e591b4c28b04a920
DEVANAND GIRI <102689281+gdev8584@users.noreply.github.com>
2022-09-04 13:15:13 +0530

    Update Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,6 @@
-import React from 'react';
+import React from "react";
 
-export const Home = () => {
-    return (
-        
-    )
-}
+const Home = () => {
+  return <div id="home-page">Home page</div>;
+};
+export default Home;
